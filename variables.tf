5b2a5ef78c5a5a996269d447c888a20786de1396
Venky <madineni70@gmail.com>
2021-09-06 17:15:22 +0530

    ci

--- variables.tf
+++ variables.tf
@@ -2,7 +2,7 @@ variable "INSTANCE_NAME" {
     default = "TonyStark"
     }
 variable "AMI_ID" {
-    default = "ami-0c1a7f89451184c8b"
+    default = "ami-092ad920d11fba91e"
     }
 variable "AWS_REGION" {
     default = "ap-south-1"
