a7bda43b0c8918e7a7617c02efb4c2643890dc0f
Venky <madineni70@gmail.com>
2021-09-06 16:38:03 +0530

    ci

--- vars.tf
+++ vars.tf
@@ -1,7 +1,7 @@
 variable "AWS_ACCESS_KEY" {}
 variable "AWS_SECRET_KEY" {}
 variable "AMI_ID" {
-    default = ami-0c1a7f89451184c8b
+    default = "ami-0c1a7f89451184c8b"
     }
 variable "AWS_REGION" {
     default = "ap-south-1"
