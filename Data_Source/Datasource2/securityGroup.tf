c01d6e466eac3890a98d41b74e00b50d138ab856
Venky <madineni70@gmail.com>
2021-09-07 00:14:37 +0530

    data

--- Data_Source/Datasource2/securityGroup.tf
+++ Data_Source/Datasource2/securityGroup.tf
@@ -9,9 +9,9 @@ resource "aws_security_group" "sg-tonyastark" {
 
     ingress = [ {
       cidr_blocks = slice(data.aws_ip_ranges.ap_south_ip_range.cidr_blocks, 0, 50)
-      from_port = 443
+      from_port = "443"
       protocol = "tcp"
-      to_port = 443
+      to_port = "443"
     } ]
     tags = {
         CreateDate = data.aws_ip_ranges.ap_south_ip_range.create_date
