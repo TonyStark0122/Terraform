a471f84621cabc65fc479c7354c034883bb3b92c
Venky <madineni70@gmail.com>
2021-09-06 23:28:44 +0530

    data

---
+++ Data_Source/createInstance.tf
@@ -0,0 +1,26 @@
+data "aws_availability_zones" "available" {}
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "tony" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  availability_zone = data.aws_availability_zones.available[2]
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
