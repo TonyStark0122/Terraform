c835cffbc5f76c63c0c7f78d2e9d28a473d0784a
Venky <madineni70@gmail.com>
2021-09-06 17:28:27 +0530

    ci

--- createInstance.tf
+++ createInstance.tf
@@ -1,9 +1,24 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
 resource "aws_instance" "web" {
-  count         = 3
-  ami           = "var.AMI_ID"
+  ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
 
   tags = {
-    Name = var.INSTANCE_NAME
+    Name = "var.INSTANCE_NAME"
   }
 }
