6d93989f68fcff006543e200aa88f1b3840715fa
Venky <madineni70@gmail.com>
2021-09-06 17:04:17 +0530

    ci

--- createInstance.tf
+++ createInstance.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "web" {
   count         = 3
   ami           = "${vars.AMI_ID}"
   instance_type = "t2.micro"
+  region        = "${vars.AWS_REGION}"
 
   tags = {
     Name = "${vars.INSTANCE_NAME}-${count.index}"
