58029908d4e1d67e6ae0f822b2fa4f2853dd3c66
Venky <madineni70@gmail.com>
2021-09-07 00:22:07 +0530

    data

--- Data_Source/Datasource2/securityGroup.tf
+++ Data_Source/Datasource2/securityGroup.tf
@@ -1,20 +1,20 @@
-data "aws_ip_ranges" "ap_south_ip_range" {
-    regions = [ "ap-south-1", "ap-east-1" ]
-    services = [ "ec2" ]
-  
+data "aws_ip_ranges" "ap_ip_range" {
+    regions = ["ap-south-1","ap-east-1"]
+    services = ["ec2"]
 }
 
-resource "aws_security_group" "sg-tonyastark" {
-    name = "starktony"
+resource "aws_security_group" "sg-custom_tony" {
+    name = "custom_tony"
 
-    ingress = [ {
-      cidr_blocks = slice(data.aws_ip_ranges.ap_south_ip_range.cidr_blocks, 0, 50)
-      from_port = "443"
-      protocol = "tcp"
-      to_port = "443"
-    } ]
+    ingress {
+        from_port = "443"
+        to_port = "443"
+        protocol = "tcp"
+        cidr_blocks = slice(data.aws_ip_ranges.ap_ip_range.cidr_blocks, 0, 50)
+    }
+
     tags = {
-        CreateDate = data.aws_ip_ranges.ap_south_ip_range.create_date
-        SyncToken = data.aws_ip_ranges.ap_south_ip_range.sync_token
+        CreateDate = data.aws_ip_ranges.ap_ip_range.create_date
+        SyncToken = data.aws_ip_ranges.ap_ip_range.sync_token
     }
 }
