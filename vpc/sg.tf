1cb37fe41af34ab6a03a72572783571fbe52d76b
Venky <madineni70@gmail.com>
2021-09-07 20:13:41 +0530

    vpc

--- vpc/sg.tf
+++ vpc/sg.tf
@@ -10,14 +10,6 @@ resource "aws_security_group" "apache" {
       to_port          = 22
       protocol         = "tcp"
       cidr_blocks      = ["0.0.0.0/0"]
-    },
-
-    {
-      description      = "http from VPC"
-      from_port        = 80
-      to_port          = 80
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
     }
   ]
 
