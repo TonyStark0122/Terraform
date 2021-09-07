635ddfc60a1ebbf70f0022c27e7df50c1b3c108e
Venky <madineni70@gmail.com>
2021-09-07 20:15:51 +0530

    vpc

--- vpc/sg.tf
+++ vpc/sg.tf
@@ -3,24 +3,19 @@ resource "aws_security_group" "apache" {
   description = "Allow ssh inbound traffic"
   vpc_id      = aws_vpc.pepper.id
 
-  ingress = [
-    {
-      description      = "ssh from VPC"
-      from_port        = 22
-      to_port          = 22
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
-    }
-  ]
+ egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-  egress = [
-    {
-      from_port        = 0
-      to_port          = 0
-      protocol         = "-1"
-      cidr_blocks      = ["0.0.0.0/0"]
-    }
-  ]
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   tags = {
     Name = "apache"
