deb793cc40e862c48516dcb104de66fd62eb04d9
Venky <madineni70@gmail.com>
2021-09-07 20:19:52 +0530

    vpc

--- vpc/sg.tf
+++ vpc/sg.tf
@@ -17,6 +17,13 @@ resource "aws_security_group" "apache" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+      from_port = 22
+      to_port = 22
+      protocol = "ssh"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = {
     Name = "apache"
   }
